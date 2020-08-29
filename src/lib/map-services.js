b6fd5dea8b9270e507fc863d6ab5880bfc7e6ef8
Adam <aalston9@gmail.com>
2020-08-29 19:39:18 -0400

    preconfigured services

--- src/lib/map-services.js
+++ src/lib/map-services.js
@@ -1,18 +1,9 @@
 import { mapServices } from 'data/map-services';
 
-/**
- * getMapServicesByName
- * @description Returns all preconfigured services
- */
-
+// get all preconfigured services
 export function getMapServices() {
   return mapServices || [];
 }
-
-/**
- * getMapServiceByName
- * @description Tries to find a service given the name. Allows additional services passed in.
- */
 
 export function getMapServiceByName( name, userServices = []) {
   const services = [...getMapServices(), ...userServices];
